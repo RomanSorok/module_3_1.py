202af98646c7b443935d5364f19fda104fa8cb37
RomanSorok <romka.22.12.kalach@gmail.com>
2024-08-01 00:56:08 +0300

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,23 @@
+import random
+
+calls = 0
+
+def count_calls():
+    global calls
+    calls += 1
+    return
+
+def string_info(string):
+    string = (len(string), string.upper(), string.lower())
+    count_calls()
+    return string
+
+def is_contains(string, list_to_search):
+    count_calls()
+    for i in list_to_search:
+        if i.lower() == string.lower():
+            return True
+    return False
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+
+print(calls)
